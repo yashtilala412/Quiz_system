23d11e0feb66f750523af9330b137e3bf7fe43a6
Yash Tilala <yashtilala412@gmail.com>
2024-07-04 10:15:04 +0530

    "database change"

--- database/config.php
+++ database/config.php
@@ -1,8 +1,8 @@
 <?php
 //database configurations
 define("DB_HOST","localhost");
-define("DB_UNAME","YOUR_DATABASE_USER_NAME");
-define("DB_PASS","YOUR_DATABASE_PASSWORD");
-define("DB_DNAME","YOUR_DATABASE_NAME");
+define("DB_UNAME","root");
+define("DB_PASS","Shyam@5000");
+define("DB_DNAME","Quiz_system");
 $conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
 ?>
