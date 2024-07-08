c6bfff5f5ab7743d135b334c7126ac57ee61a9e9
yash <yashtilala412@gmail.com>
2024-07-08 16:25:24 +0530

    "smtp config"

---
+++ admin/files/smtpconfig.php
@@ -0,0 +1,7 @@
+// smtp_config.php
+<?php
+define('SMTP_HOST', 'your_smtp_host');
+define('SMTP_PORT', 587); // or 465 for SSL
+define('SMTP_USER', 'your_email@example.com');
+define('SMTP_PASS', 'your_email_password');
+?>
