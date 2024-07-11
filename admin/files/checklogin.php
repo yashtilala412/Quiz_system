cb3bbb178c9f3fb7c0f341702e8e4138a78dd132
yash <yashtilala412@gmail.com>
2024-07-11 11:52:05 +0530

    changes1

--- admin/files/checklogin.php
+++ admin/files/checklogin.php
@@ -5,7 +5,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 	include "../../database/config.php";
 	$username=$_POST["username"];
 	$password=$_POST["password"];
-	$enc_password=hash('sha256',$password,false);
+	$enc_password=$password;
 	$sql="SELECT * from teachers where email='$username' AND password='$enc_password'";
 	$res=mysqli_query($conn,$sql);
 	if(mysqli_num_rows($res) == 1)
--- database/config.php
+++ database/config.php
@@ -3,6 +3,6 @@
 define("DB_HOST","localhost");
 define("DB_UNAME","root");
 define("DB_PASS","Shyam@5000");
-define("DB_DNAME","Quiz_system");
+define("DB_DNAME","quiz_system");
 $conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
 ?>
