620dff8e4f037640cfc2023a2d98cc8dd8c5956a
yash <yashtilala412@gmail.com>
2024-07-11 15:42:22 +0530

    config added

--- admin/files/add_extra_student.php
+++ admin/files/add_extra_student.php
@@ -5,6 +5,7 @@ use PHPMailer\PHPMailer\Exception;
 require 'path/to/PHPMailer/src/Exception.php';
 require 'path/to/PHPMailer/src/PHPMailer.php';
 require 'path/to/PHPMailer/src/SMTP.php';
+include "../../database/config.php";
 
     $id;
 	include "../../database/config.php";
