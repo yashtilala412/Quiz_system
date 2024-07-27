4333cd47786bf101bac2e8dba25052235a108680
yash <yashtilala412@gmail.com>
2024-07-27 14:56:14 +0530

    final code add

--- admin/files/displaytime.php
+++ admin/files/displaytime.php
@@ -1,14 +1,26 @@
+<?php
+// Fetch test details
+$test_id = $_GET['test_id'];
+$sql = "SELECT * FROM tests WHERE id = '$test_id'";
+$result = mysqli_query($conn, $sql);
+$test = mysqli_fetch_assoc($result);
+
 // Fetch test creator details
 $creator_id = $test['creator_id'];
 $sql_creator = "SELECT * FROM users WHERE id = '$creator_id'";
 $result_creator = mysqli_query($conn, $sql_creator);
 $creator = mysqli_fetch_assoc($result_creator);
 
-// Display creator details
-echo "<p>Creator: {$creator['name']}</p>";
+// Display test details
+echo "<h1>{$test['name']}</h1>";
+echo "<p>Date: {$test['date']}</p>";
+echo "<p>Subject: {$test['subject']}</p>";
+echo "<p>Total Questions: {$test['total_questions']}</p>";
+echo "<p>Time Limit: {$test['time_limit']} minutes</p>";
 echo "<p>Description: {$test['description']}</p>";
+echo "<p>Creator: {$creator['name']}</p>";
 echo "<p><a href='{$test['resource_link']}' target='_blank'>Related Resources</a></p>";
-echo "<a href='download_test.php?test_id={$test_id}' class='btn btn-primary'>Download Test</a>";
+
 if ($test['time_limit'] > 0) {
     echo "<div id='countdown'></div>";
     echo "<script>
@@ -26,8 +38,10 @@ if ($test['time_limit'] > 0) {
     updateTimer();
     </script>";
 }
-echo "<p>Instructions: {$test['instructions']}</p>";
+
+echo "<a href='download_test.php?test_id={$test_id}' class='btn btn-primary'>Download Test</a>";
 echo "<a href='start_test.php?test_id={$test_id}' class='btn btn-success'>Start Test</a>";
+
 $sql_questions = "SELECT * FROM questions WHERE test_id = '$test_id'";
 $result_questions = mysqli_query($conn, $sql_questions);
 
@@ -36,7 +50,9 @@ while ($question = mysqli_fetch_assoc($result_questions)) {
     echo "<li>{$question['question_text']}</li>";
 }
 echo "</ul>";
+
 echo "<a href='view_results.php?test_id={$test_id}' class='btn btn-info'>View Results</a>";
+
 echo "<h3>Feedback:</h3>";
 echo "<form action='submit_feedback.php' method='post'>
     <input type='hidden' name='test_id' value='{$test_id}'>
@@ -44,3 +60,4 @@ echo "<form action='submit_feedback.php' method='post'>
     <br>
     <input type='submit' value='Submit Feedback'>
 </form>";
+?>
