659584feb289f81134ed2de8513f2ee0a5a9e5e0
yash <yashtilala412@gmail.com>
2024-07-27 14:59:39 +0530

    search by key word

--- admin/files/addquestionbank.php
+++ admin/files/addquestionbank.php
@@ -1,14 +1,8 @@
-$categories = ['Math', 'Science', 'History']; // Example categories
 echo "<form method='GET' action='test_view.php'>
-    <label for='category'>Filter by Category:</label>
-    <select name='category' id='category'>
-        <option value=''>All</option>";
-foreach ($categories as $category) {
-    echo "<option value='{$category}'>{$category}</option>";
-}
-echo "</select>
-    <input type='submit' value='Filter'>
+    <label for='keyword'>Search Questions:</label>
+    <input type='text' name='keyword' id='keyword'>
+    <input type='submit' value='Search'>
 </form>";
 
-$category_filter = isset($_GET['category']) ? $_GET['category'] : '';
-$sql_questions = "SELECT * FROM question_bank" . ($category_filter ? " WHERE category = '$category_filter'" : "");
+$keyword_filter = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+$sql_questions = "SELECT * FROM question_bank" . ($keyword_filter ? " WHERE question_text LIKE '%$keyword_filter%'" : "");
