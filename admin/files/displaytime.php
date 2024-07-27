ccb03285f7779adae0e075c1c10961afee717585
yash <yashtilala412@gmail.com>
2024-07-27 14:53:22 +0530

    display test monitor

--- admin/files/displaytime.php
+++ admin/files/displaytime.php
@@ -9,3 +9,20 @@ echo "<p>Creator: {$creator['name']}</p>";
 echo "<p>Description: {$test['description']}</p>";
 echo "<p><a href='{$test['resource_link']}' target='_blank'>Related Resources</a></p>";
 echo "<a href='download_test.php?test_id={$test_id}' class='btn btn-primary'>Download Test</a>";
+if ($test['time_limit'] > 0) {
+    echo "<div id='countdown'></div>";
+    echo "<script>
+    var timeLimit = {$test['time_limit'] * 60}; // Convert minutes to seconds
+    var countdown = document.getElementById('countdown');
+    function updateTimer() {
+        var minutes = Math.floor(timeLimit / 60);
+        var seconds = timeLimit % 60;
+        countdown.textContent = minutes + 'm ' + seconds + 's';
+        if (timeLimit > 0) {
+            timeLimit--;
+            setTimeout(updateTimer, 1000);
+        }
+    }
+    updateTimer();
+    </script>";
+}
