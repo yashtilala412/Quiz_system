8e888269232d8330ccbb4264e6bff85b9d637b52
yash <yashtilala412@gmail.com>
2024-07-31 11:15:43 +0530

    configuratrion

--- admin/files/get_classes.php
+++ admin/files/get_classes.php
@@ -1,40 +1,9 @@
 <?php
-function log_message($message) {
-    $log_file = 'app.log';
-    $current_time = date('Y-m-d H:i:s');
-    file_put_contents($log_file, "$current_time - $message\n", FILE_APPEND);
-}
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
 
-$info = [];
-include "../../database/config.php";
-
-// Check connection
-if ($conn->connect_error) {
-    log_message("Connection failed: " . $conn->connect_error);
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$stmt = $conn->prepare("SELECT name FROM classes");
-$stmt->execute();
-$result = $stmt->get_result();
-
-if (!$result) {
-    log_message("Query failed: " . mysqli_error($conn));
-    die("Query failed: " . mysqli_error($conn));
-}
-
-log_message("Query successful");
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        $info[] = $row['name'];
-    }
-    echo json_encode($info);
-} else {
-    echo "0 results";
-}
-
-$stmt->close();
-mysqli_close($conn);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 ?>
