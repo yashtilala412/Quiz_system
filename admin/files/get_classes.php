3043ab51f353a9dd84e5709a8f4f0e3eeecae4ca
yash <yashtilala412@gmail.com>
2024-07-31 11:13:03 +0530

    error changes and handling4

--- admin/files/get_classes.php
+++ admin/files/get_classes.php
@@ -1,19 +1,28 @@
 <?php
-    $info;
-	include "../../database/config.php";
-   
-        $classes = "SELECT name FROM classes ";
-        $result = mysqli_query($conn, $classes);
-                
-        if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($result)) {
-                $info[] = $row['name'];
-            }
-            echo json_encode($info);
-        } else {
-            echo "0 results";
+    $info = [];
+    include "../../database/config.php";
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $classes = "SELECT name FROM classes";
+    $result = mysqli_query($conn, $classes);
+
+    if (!$result) {
+        die("Query failed: " . mysqli_error($conn));
+    }
+    
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            $info[] = $row['name'];
         }
+        echo json_encode($info);
+    } else {
+        echo "0 results";
+    }
 
     mysqli_close($conn);
 ?>
