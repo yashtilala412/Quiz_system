db73c56faa545f236bb9b952dda3cb4528e7c610
yash <yashtilala412@gmail.com>
2024-08-22 19:19:53 +0530

    senetize json data

--- files/get_dashboard_contents.php
+++ files/get_dashboard_contents.php
@@ -83,6 +83,8 @@ if (!isset($_SESSION['last_activity']) || (time() - $_SESSION['last_activity']) 
     exit();
 }
 $_SESSION['last_activity'] = time();
+$data = htmlspecialchars($_SESSION['student_details'], ENT_QUOTES, 'UTF-8');
+logMessage("Sanitized session data: " . $data);
 
 mysqli_close($conn);
 logMessage("Script execution ended.");
