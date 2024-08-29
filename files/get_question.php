d63660f3ff6c7a642064b5e6e150e9b8e5c12cfe
yash <yashtilala412@gmail.com>
2024-08-29 07:56:51 +0530

    progress percentage1

--- files/get_question.php
+++ files/get_question.php
@@ -153,13 +153,9 @@ if (isset($_POST['reset_quiz'])) {
 }
 echo '<button onclick="resetQuiz()">Reset Quiz</button>';
 // Feature 14: AJAX-based question loading
-echo '<script>
-    function loadNextQuestion() {
-        fetch("fetch_question.php").then(response => response.json()).then(data => {
-            document.getElementById("questionContainer").innerHTML = data.question;
-        });
-    }
-</script>';
+// Feature 15: Log IP address for each user action
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+file_put_contents('user_actions.log', "User IP: $ipAddress - Action Time: " . date("Y-m-d H:i:s") . "\n", FILE_APPEND);
 
 
 
