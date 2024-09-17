2f9d15622e90a2e045e9a42fc862b5ef67a42737
yash <yashtilala412@gmail.com>
2024-09-17 16:01:23 +0530

    filter multiple providers

--- files/student_login.php
+++ files/student_login.php
@@ -10,22 +10,21 @@
         $student_roll_number = $_POST['rollNumber'];
         $student_password = $_POST['password'];
 
-        // Function to get the user's IP address
-        function getUserIP() {
-            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-                $ip = $_SERVER['HTTP_CLIENT_IP'];
-            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            } else {
-                $ip = $_SERVER['REMOTE_ADDR'];
-            }
-            
-            // Log IP address
-            file_put_contents('ip_log.txt', $ip . PHP_EOL, FILE_APPEND);
-            
-            return $ip;
-        }
-        
+        // Function to get the user's IP addressfunction getUserIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $ip = trim($ipList[0]); // Get the first IP in the list
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    
+    file_put_contents('ip_log.txt', $ip . PHP_EOL, FILE_APPEND);
+    
+    return $ip;
+}
+
 
         // Log the IP address
         $user_ip = getUserIP();
